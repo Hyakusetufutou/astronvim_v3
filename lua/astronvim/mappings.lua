f582365664fe631f200b0a912ef504e22cd416a5
Hyakusetufutou <Hyakusetufutou358@gmail.com>
2024-10-08 06:53:12 +0900

    git関連のプラグインを追加

--- lua/astronvim/mappings.lua
+++ lua/astronvim/mappings.lua
@@ -162,6 +162,7 @@ if is_available "gitsigns.nvim" then
   maps.n["<leader>gS"] = { function() require("gitsigns").stage_buffer() end, desc = "Stage Git buffer" }
   maps.n["<leader>gu"] = { function() require("gitsigns").undo_stage_hunk() end, desc = "Unstage Git hunk" }
   maps.n["<leader>gd"] = { function() require("gitsigns").diffthis() end, desc = "View Git diff" }
+  maps.n["<leader>gg"] = { function() require("gitgraph").draw({}, {all = true, max_count = 5000}) end, desc = "GitGraph" }
 end
 
 -- NeoTree
--- lua/plugins/gitextension.lua
+++ lua/plugins/gitextension.lua
@@ -1,9 +1,67 @@
 return {
+  -- {
+  --   "tpope/vim-fugitive",
+  --   cmd = {"G", "Git", "Git diff", "Gdiffsplit","Gvdiffsplit", "Glog", "Gclogu" },
+  --   config = function ()
+  --     
+  --   end
+  -- },
+  -- {
+  --   'dinhhuy258/git.nvim',
+  --   cmd = {"GitDiff"},
+  --   config = function()
+  --     require('git').setup()
+  --   end,
+  -- },
   {
-    "tpope/vim-fugitive",
-    cmd = {"G", "Git", "Git diff"},
-    config = function ()
-      
-    end
-  }
+    "sindrets/diffview.nvim",
+    event = "User AstroGitFile",
+    cmd = { "DiffviewOpen" },
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        default = { winbar_info = true },
+        file_history = { winbar_info = true },
+      },
+      hooks = { diff_buf_read = function(bufnr) vim.b[bufnr].view_activated = false end },
+    },
+    specs = {
+      {
+        "NeogitOrg/neogit",
+        optional = true,
+        opts = { integrations = { diffview = true } },
+      },
+    },
+  },
+  {
+    "isakbm/gitgraph.nvim",
+    lazy = true,
+    opts = {
+      symbols = {
+        merge_commit = '●',
+        commit = '*',
+      },
+      format = {
+        timestamp = '%Y-%m-%d %H:%M:%S ',
+        fields = { 'hash', 'timestamp', 'author', 'branch_name', 'tag' },
+      },
+    },
+    specs = {
+      { -- use diffview for viewing commits if available
+        "sindrets/diffview.nvim",
+        optional = true,
+        specs = {
+          {
+            "isakbm/gitgraph.nvim",
+            opts = {
+              hooks = {
+                on_select_commit = function(commit) vim.cmd.DiffviewOpen(commit.hash .. "^!") end,
+                on_select_range_commit = function(from, to) vim.cmd.DiffviewOpen(from.hash .. "~1.." .. to.hash) end,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 }
